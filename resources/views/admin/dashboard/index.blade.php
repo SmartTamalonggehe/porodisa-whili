b0f8bff13d2e639972d72eace930ddc086c2af67
SmartTamalonggehe <smartspartacus@gmail.com>
2022-06-09 18:39:46 +0900

    mengganti tampilan dashboard pimpinan

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -1,36 +1,39 @@
-
 @extends('admin.layouts.default')
 
-@section('judul','Dashboard')
+@section('judul', 'Dashboard')
 
 @section('main')
 
 
-	<!-- Main content -->
-	<section class="content">
-			<div class="row">
-				<div class="col-xl-12 col-12">
-					<div class="box bg-primary">
-						<div class="box-body d-flex px-0">
-							<div class="flex-grow-1 p-30 flex-grow-1 bg-img dask-bg bg-none-md" style="background-position: right bottom; background-size: auto 100%;  " > <img src="{{asset('assets_admin/images/svg-icon/color-svg/custom-1.svg')}}" alt="" srcset="">
-								<div class="row">
-									<div class="col-12 col-xl-7">
-										<h2>Selamat Datang Kembali, <strong>Admin!</strong></h2>
+    <!-- Main content -->
+    <section class="content">
+        <div class="row">
+            <div class="col-xl-12 col-12">
+                <div class="box bg-primary">
+                    <div class="box-body d-flex px-0">
+                        <div class="flex-grow-1 p-30 flex-grow-1 bg-img dask-bg bg-none-md"
+                            style="background-position: right bottom; background-size: auto 100%;  "> <img
+                                src="{{ asset('assets_admin/images/svg-icon/color-svg/custom-1.svg') }}" alt="" srcset="">
+                            <div class="row">
+                                <div class="col-12 col-xl-7">
+                                    <h2>Selamat Datang Kembali, <strong>Admin!</strong></h2>
 
-									</div>
-									<div class="col-12 col-xl-5"></div>
-								</div>
-							</div>
-						</div>
-					</div>				
-			</div>
-		</section>
-		<!-- /.content -->
+                                </div>
+                                <div class="col-12 col-xl-5"></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- /.content -->
 
-   
+
 
 @endsection
 
 
 </body>
+
 </html>
